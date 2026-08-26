627f711b70a6341cb10169fb1fba0fce605529fc
tayyaba-official <312965806+tayyaba-official@users.noreply.github.com>
2026-08-26 23:46:07 +0500

    Add progressive sequence character triangle pattern code in C++.

---
+++ loops/while-loop/patterns/16-char-triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n; // Take input for total rows
+    int i = 1; // Outer loop for rows
+    while(i <= n){
+        int j = 1; // Reset inner loop counter for every new row
+         
+        while(j <= i){
+            char value = ('A'+i+j-2);
+            cout << value << " "; // Print the character with a space
+            j++; // Increment inner loop counter
+        }
+        
+        cout << endl; // Move to the next line after completing a row
+        i++; // Increment outer loop counter
+    }
+    
+    return 0;
+}
